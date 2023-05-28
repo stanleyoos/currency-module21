7639977d15e42b1ad5dfbd9f16c209a5d6d7bedc
stanleyos <stanislaw.sarka@gmail.com>
2023-05-28 15:18:24 -0500

    Add tests to convertions in ResultBox component

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,26 +1,28 @@
-import PropTypes from 'prop-types';
-import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
-import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
-import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
-import { useMemo } from 'react';
-import styles from './ResultBox.module.scss';
+import PropTypes from "prop-types"
+import { convertUSDToPLN } from "./../../utils/convertUSDToPLN"
+import { convertPLNToUSD } from "./../../utils/convertPLNToUSD"
+import { formatAmountInCurrency } from "./../../utils/formatAmountInCurrency"
+import { useMemo } from "react"
+import styles from "./ResultBox.module.scss"
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
-    return formatAmountInCurrency(amount, from);
-  }, [from, to, amount]);
+    if (from === "USD" && to === "PLN") return convertUSDToPLN(amount)
+    if (from === "PLN" && to === "USD") return convertPLNToUSD(amount)
+    return formatAmountInCurrency(amount, from)
+  }, [from, to, amount])
 
-  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
+  const formattedAmount = useMemo(
+    () => formatAmountInCurrency(amount, from),
+    [amount, from]
+  )
 
   return (
-    <div className={styles.result}>
+    <div className={styles.result} data-testid="resultBoxDiv">
       {formattedAmount} = {convertedAmount}
     </div>
-  );
-};
+  )
+}
 
 ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
@@ -28,4 +30,4 @@ ResultBox.propTypes = {
   amount: PropTypes.number.isRequired,
 }
 
-export default ResultBox;
+export default ResultBox
